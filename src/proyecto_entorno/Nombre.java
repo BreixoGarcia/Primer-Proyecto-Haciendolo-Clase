cd1db38f412b63fc4305e51fcfc689088ecd96fa
BreixoGarcia <breixogarcia490@gmail.com>
2025-12-02 13:23:02 +0100

    Actualizamos el nombre

--- src/proyecto_entorno/Nombre.java
+++ src/proyecto_entorno/Nombre.java
@@ -5,7 +5,7 @@ public class Nombre {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Breixo Garcia Canovacas");
+		System.out.println("Breixo");
 	}
 
 }
