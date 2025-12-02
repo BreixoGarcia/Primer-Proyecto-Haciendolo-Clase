6dcdbf24acc8a5923d31eab90d89c7e41fd5287f
BreixoGarcia <breixogarcia490@gmail.com>
2025-12-02 13:09:43 +0100

    Primer Commit
    Primer commit con la clase ciclo.java y nombre.java

---
+++ src/proyecto_entorno/Ciclo.java
@@ -0,0 +1,11 @@
+package proyecto_entorno;
+
+public class Ciclo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Desarrollo en aplicaciones web");
+	}
+
+}
---
+++ src/proyecto_entorno/Nombre.java
@@ -0,0 +1,11 @@
+package proyecto_entorno;
+
+public class Nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Breixo Garcia Canovacas");
+	}
+
+}
